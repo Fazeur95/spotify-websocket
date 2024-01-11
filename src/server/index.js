ab7954a5cebe80bf83e94b0326a2083c15cde1f1
Faouizi MZEBLA <faouizi.mzebla@ynov.com>
2024-01-11 15:22:26 +0100

    Add Socket.IO event handlers for pause, resume, and stop sound

--- src/server/index.js
+++ src/server/index.js
@@ -15,6 +15,7 @@ const io = socketIo(server, {
 const port = process.env.PORT || 8083;
 
 // Gestion des connexions Socket.IO
+// Gestion des connexions Socket.IO
 io.on('connection', socket => {
   console.log('User connected');
 
@@ -23,8 +24,25 @@ io.on('connection', socket => {
     console.log('playSound', data);
     io.emit('playSound', data);
   });
-});
 
+  // Gestion de l'événement 'pauseSound'
+  socket.on('pauseSound', () => {
+    console.log('pauseSound');
+    io.emit('pauseSound');
+  });
+
+  // Gestion de l'événement 'resumeSound'
+  socket.on('resumeSound', () => {
+    console.log('resumeSound');
+    io.emit('resumeSound');
+  });
+
+  // Gestion de l'événement 'stopSound'
+  socket.on('stopSound', () => {
+    console.log('stopSound');
+    io.emit('stopSound');
+  });
+});
 // Démarrage du serveur
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
