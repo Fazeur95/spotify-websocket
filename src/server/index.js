0ece3632e3144240d63b1e457c7dc8c171393d17
Faouizi MZEBLA <faouizi.mzebla@ynov.com>
2024-01-11 16:04:59 +0100

    Remove unused sound control events

--- src/server/index.js
+++ src/server/index.js
@@ -24,24 +24,6 @@ io.on('connection', socket => {
     console.log('playSound', data);
     io.emit('playSound', data);
   });
-
-  // Gestion de l'événement 'pauseSound'
-  socket.on('pauseSound', () => {
-    console.log('pauseSound');
-    io.emit('pauseSound');
-  });
-
-  // Gestion de l'événement 'resumeSound'
-  socket.on('resumeSound', () => {
-    console.log('resumeSound');
-    io.emit('resumeSound');
-  });
-
-  // Gestion de l'événement 'stopSound'
-  socket.on('stopSound', () => {
-    console.log('stopSound');
-    io.emit('stopSound');
-  });
 });
 // Démarrage du serveur
 server.listen(port, () => {
